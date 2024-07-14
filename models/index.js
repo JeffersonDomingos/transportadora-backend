d83021c83ac5d3e4c0810d4956e0a36266936371
JeffersonDomingos <dominguesmorais@hotmail.com>
2024-07-14 01:36:39 -0300

    fix: postgresq to vercel

--- models/index.js
+++ models/index.js
@@ -5,6 +5,7 @@ const sequelize = new Sequelize({
   dialect: 'postgres',
   dialectModule: require('pg'),
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
